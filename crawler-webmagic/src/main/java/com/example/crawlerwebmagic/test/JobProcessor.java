08bfa2eca74b06c69d01ea6186de2fb6f9950bf2
zhengzhendu <zdzhengzhendu@163.com>
2021-01-23 15:09:41 +0800

    使用css选择器，xpath,正则表达式获取元素

--- crawler-webmagic/src/main/java/com/example/crawlerwebmagic/test/JobProcessor.java
+++ crawler-webmagic/src/main/java/com/example/crawlerwebmagic/test/JobProcessor.java
@@ -14,8 +14,17 @@ public class JobProcessor implements PageProcessor {
      */
     @Override
     public void process(Page page) {
+        //css选择器
         //解析返回的数据page,并且把解析的结果放到ResultItem中
         page.putField("div", page.getHtml().css("div.cw-icon a").all());
+
+
+        // Xpath
+        page.putField("div2", page.getHtml().xpath("//li[@id=ttbar-home]/a"));
+
+        //正则表达式
+        page.putField("div3", page.getHtml().css("div a").regex(".*京东.*").all());
+
 
     }
 
