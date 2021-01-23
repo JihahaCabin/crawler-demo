37dc42764bb3e6ddf04cf996afb1f28ac28de1a9
zhengzhendu <zdzhengzhendu@163.com>
2021-01-23 15:36:14 +0800

    使用addTargetRequests，获取页面中的链接，继续发起请求

--- crawler-webmagic/src/main/java/com/example/crawlerwebmagic/test/JobProcessor.java
+++ crawler-webmagic/src/main/java/com/example/crawlerwebmagic/test/JobProcessor.java
@@ -16,16 +16,22 @@ public class JobProcessor implements PageProcessor {
     public void process(Page page) {
         //css选择器
         //解析返回的数据page,并且把解析的结果放到ResultItem中
-        page.putField("div", page.getHtml().css("div.cw-icon a").all());
+        page.putField("div", page.getHtml().css("div h2").all());
 
 
         // Xpath
         page.putField("div2", page.getHtml().xpath("//li[@id=ttbar-home]/a"));
 
         //正则表达式
-        page.putField("div3", page.getHtml().css("div a").regex(".*京东.*").all());
+        page.putField("div3", page.getHtml().css("div h2").regex(".*常用.*").all());
 
+        //处理结果的api get(),toString()获取第一条数据，all()获取所有数据
+        page.putField("div4", page.getHtml().css("div a").regex(".*京东.*").get());
+        page.putField("div5", page.getHtml().css("div a").regex(".*京东.*").toString());
 
+        //获取链接
+        page.addTargetRequests(page.getHtml().css("div.topicCard__body").links().all());
+        page.putField("url", page.getHtml().css("div a").all());
     }
 
 
@@ -43,7 +49,7 @@ public class JobProcessor implements PageProcessor {
     //主函数执行爬虫
     public static void main(String[] args) {
         Spider.create(new JobProcessor())
-                .addUrl("https://kuaibao.jd.com/") //设置要爬取的页面
+                .addUrl("https://helpcenter.jd.com/") //设置要爬取的页面
                 .run();
     }
 }
